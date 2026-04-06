984e925085edbc7391989e454c1ae3a2ec0d3936
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-06 16:51:59 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,23 +4,21 @@ export function Footer() {
   return (
     <footer className="siteFooter">
       <div className="container footerInner">
-        <div className="footerLeft">
-          <div className="footerBrand">Callie</div>
-          <div className="footerMini">
-            Your events, on everyone&apos;s phone&nbsp;✨
+        <div className="footerRow">
+          <div className="footerLeft">
+            <div className="footerBrand">Callie</div>
+            <div className="footerMini">
+              Your events, on everyone&apos;s phone&nbsp;✨
+            </div>
           </div>
-        </div>
-
-        <div className="footerRight">
           <a className="footerLink" href="mailto:hello@callietools.com">
             hello@callietools.com
           </a>
-          <span className="footerDot">&middot;</span>
+        </div>
+        <div className="footerRow">
           <span className="footerUpgrade">
-            Want your own logo and colors?{" "}
-            <Link href="/upgrade">
-              Make it yours &mdash; $10/mo
-            </Link>
+            Want your own logo and colors on your calendar page?{" "}
+            <Link href="/upgrade">Make it yours &mdash; $10/mo</Link>
           </span>
         </div>
       </div>
