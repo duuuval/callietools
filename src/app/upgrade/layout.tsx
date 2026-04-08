c8c7293fe49f5dd79584be5f4804aae2cb15fd0f
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-08 12:43:02 -0400

    Create upgrade/layout.tsx

---
+++ src/app/upgrade/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/Header";
+
+export default function UpgradeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <main className="main">{children}</main>
+    </>
+  );
+}
