d2142247e64b26b45734be8c77e3160540555588
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-08 12:50:19 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export function Footer() {
+  const pathname = usePathname();
+  const isUpgrade = pathname === "/upgrade";
+
   return (
     <footer className="siteFooter">
       <div className="container footerInner">
@@ -7,19 +14,21 @@ export function Footer() {
           <div className="footerLeft">
             <div className="footerBrand">Callie</div>
             <div className="footerMini">
-              Your events, on everyone&apos;s phone&nbsp;✨
+              Your events, on everyone&apos;s phone
             </div>
           </div>
           <a className="footerLink" href="mailto:hello@callietools.com">
             hello@callietools.com
           </a>
         </div>
-        <div className="footerRow">
-          <span className="footerUpgrade">
-            Want your own logo and colors on your calendar page?{" "}
-            <Link href="/upgrade">Make it yours &mdash; $10/mo</Link>
-          </span>
-        </div>
+        {!isUpgrade && (
+          <div className="footerRow">
+            <span className="footerUpgrade">
+              Want your own logo and colors on your calendar page?{" "}
+              <Link href="/upgrade">Make it yours &mdash; $10/mo</Link>
+            </span>
+          </div>
+        )}
       </div>
     </footer>
   );
