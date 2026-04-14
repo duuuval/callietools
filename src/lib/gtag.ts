1c56007ab970c82b376d96f4528ee220102e68db
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-14 16:46:33 -0400

    Create gtag.ts

---
+++ src/lib/gtag.ts
@@ -0,0 +1,7 @@
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+
+export function trackEvent(name: string, params?: Record<string, string | number | boolean>) {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', name, params);
+  }
+}
