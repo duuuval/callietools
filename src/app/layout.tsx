620baf964929027eba074f8e4407dffd551bd9f0
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-14 16:49:37 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics";
 
+
 export const metadata: Metadata = {
   title: {
     default: "Callie",
