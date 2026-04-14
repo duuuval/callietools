1929142f957cab9d46d51b99c6d73325409bb118
duuuval <129026276+duuuval@users.noreply.github.com>
2026-04-14 16:48:36 -0400

    Update gtag.ts

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
 export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 
 export function trackEvent(name: string, params?: Record<string, string | number | boolean>) {
